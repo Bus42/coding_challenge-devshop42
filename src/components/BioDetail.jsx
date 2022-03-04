6c1a5dae0191779078f32f77ec579f3e3b4e5477
bus42 <gbrewton1980@gmail.com>
2022-03-04 15:41:34 -0700

    Implements navigation and screen

--- src/components/BioDetail.jsx
+++ src/components/BioDetail.jsx
@@ -1,20 +1,47 @@
-const BioDetail = (props) => {
-    const {bio, name, title, img_url} = props.bioData;
-    return ( 
-        <div id="bio-detail" >
-            <div className="bio-detail_wrapper">
-                <img className="bioDetail-image" src={img_url} alt={name} />
-                <div className="bio-detail_content">
-                    <div className="bio-detail_top">
-                        <h3>{name}</h3>
-                        <h6>{title}</h6>
-                    </div>
-                    <div className="bio-detail_bottom">
-                        <p>{bio.replace(/(<p>)/, "").replace(/<\/p>/, "")}</p>
-                    </div>
-                </div>
-             </div>   
-        </div> );
-}
- 
+import { useLayoutEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { documents } from "../data";
+import Home from "./Home";
+
+const BioDetail = () => {
+  const { id } = useParams();
+  const [bioCard, setBioCard] = useState({});
+  useLayoutEffect(() => {
+    setBioCard(documents.find((card) => card._id === id));
+  }, [id]);
+
+  const handleClick = () => {
+    window.history.back();
+  };
+
+  return (
+    <>
+      <Home />
+      <div id="screen" />
+      <div id="bio-detail">
+        {bioCard ? (
+          <div className="bio-detail_wrapper" onClick={handleClick}>
+            <img
+              className="bioDetail-image"
+              src={bioCard.photo_url}
+              alt={bioCard.name}
+            />
+            <div className="bio-detail_content">
+              <div className="bio-detail_top">
+                <h3>{bioCard.name}</h3>
+                <h6>{bioCard.title}</h6>
+              </div>
+              <div className="bio-detail_bottom">
+                <p>{bioCard.bio?.replace(/(<p>)/, "").replace(/<\/p>/, "")}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </>
+  );
+};
+
 export default BioDetail;
