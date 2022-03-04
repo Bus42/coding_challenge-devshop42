97265ef1ea31ea777f0ae2daedc13faba723b9e6
bus42 <gbrewton1980@gmail.com>
2022-03-04 15:00:47 -0700

    Implements router in App, isolates Home component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
-import { useEffect, useState } from 'react';
+import Home from "./components/Home";
+import BioDetail from "./components/BioDetail";
 import './App.css';
-import BioCard from './components/BioCard';
-import Header from "./components/Header";
 // import axios from 'axios';
-import { documents } from "./data";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
-  const [bioCards, setBioCards] = useState([]);
-  useEffect(() => {
-    //   const instance = axios.create({
-    //     baseURL: "https://data.mongodb-api.com/app/data-uivri/endpoint/data/beta/action",
-    //     headers: {
-    //       "Accept": "*/*",
-    //       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    //       "Content-Type": "application/json",
-    //       "Access-Control-Request-Headers": "*",
-    //       "api-key": "oSYA2hyWQN4w7AbywmlzcdvcgjiVhQLq5pixbpCREOa63glNzC2AXidaNWUvSdoR"
-    //     }
-    //   });
-
-    //   const body = JSON.stringify({
-    //     "collection": "data",
-    //     "database": "CodeChallenge",
-    //     "dataSource": "DevShop24"
-    // })
-
-    //   instance.post("/find", body)
-    //     .then(res => {
-    //       setBioCards(res.data.data);
-    //     })
-    //     .catch(err => {
-    //       console.log(err);
-    //     }
-    //     );
-  }, []);
-
-  useEffect(() => {
-    setBioCards(documents);
-  }, [])
-
   return (
+
     <div id="App-wrapper">
-      <div id="content-wrapper">
-        <Header />
-        {bioCards.map((card, index) => <BioCard cardData={card} key={index} />)}
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<BioDetail />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
